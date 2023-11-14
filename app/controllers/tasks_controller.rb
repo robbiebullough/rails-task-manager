5fd9f569c2e37f836a85b656b536ff5427be791f
Robbie Bullough <robbie.bullough@hotmail.co.uk>
2023-11-14 12:52:37 +0000

    Added basic functionality for app without any styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = set_task
+  end
+
+  def update
+    @task = set_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = set_task
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @restaurant = Task.find(params[:id])
+  end
 end
